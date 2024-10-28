ef60d431642f37339cfa7a2df6b1df3014f1b1af
Roman <banan12.github@senthy.com>
2024-10-29 02:07:12 +0300

    fixed custom color saving

--- src/ui/background/IconBackground.tsx
+++ src/ui/background/IconBackground.tsx
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from 'react';
 import {IconType} from "react-icons";
+import Cookies from 'js-cookie';
 
 interface IconBackgroundProps {
     icons: IconType[];
     iconSize?: number;
     iconOpacity?: number;
-    backgroundColor:string;
 }
 
 const IconBackground: React.FC<IconBackgroundProps> = ({icons, iconSize = 24, iconOpacity = 0.1,}) => {
+    const storedColor = Cookies.get('bgColor');
+    const [customColor,] = useState<string>(storedColor || '#14151B');
+    console.log('custom bg color', customColor);
+
     function getRandomPosition(minOffset = 0, maxOffset = 100): number {
         return Math.floor(Math.random() * (maxOffset - minOffset + 1)) + minOffset;
     }
 
+    console.log("className:", `fixed inset-0 -z-10 overflow-hidden ${customColor && customColor != '' ? `bg-[${customColor}]` : 'dark:bg-[#14151B]'}`);
+
     return (
-        <div className="fixed inset-0 -z-10 overflow-hidden dark:bg-[#14151B]">
+        <div className={`fixed inset-0 -z-10 overflow-hidden dark:bg-[#14151B]`}>
             <div className="grid grid-cols-12 grid-rows-12 gap-0 h-full w-full">
                 {Array.from({length: 12 * 12}).map((_, index) => (
                     <div
