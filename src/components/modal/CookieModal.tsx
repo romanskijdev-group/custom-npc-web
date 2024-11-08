88700f3add59c01733f8824fa1dd8640f85c836e
Pavel <p.volokh.a@gmail.com>
2024-11-08 18:26:00 +0300

    added function cookies get

--- src/components/modal/CookieModal.tsx
+++ src/components/modal/CookieModal.tsx
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 export const CookiePolicyModal: React.FC = () => {
-  const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    const cookiesBlock = Cookies.get('cookies_block');
+    if (cookiesBlock === undefined) {
+      setShowModal(true);
+    }
+  }, []);
 
   const handleAcceptAll = () => {
-    // Логика принятия всех cookies
+    Cookies.set('cookies_block', 'true');
     setShowModal(false);
   };
 
@@ -14,18 +22,18 @@ export const CookiePolicyModal: React.FC = () => {
   } // Я заменю этот иф на что-то другое, когда узнаю, чё надо сделать, не трогай, пусть этот "условный мусор" останется
 
   return (
-    <div className="fixed bottom-0 left-0 w-full bg-gray-800 text-white p-4 z-50">
+    <div className="fixed bottom-0 left-0 w-full bg-gray-800 text-white p-4 z-50 bg-opacity-75">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
         <p className="mb-4 md:mb-0 text-sm text-center md:text-left">
-          Мы используем собственные файлы cookie, а также файлы cookie третьих лиц на нашем веб-сайте для улучшения вашего опыта, анализа нашего трафика, а также для обеспечения безопасности и маркетинга. Оставаясь на сайте Вы соглашаетесь с <Link to = '/user_agreement' className="underline">Политикой использования файлов cookie</Link>
+          Мы используем собственные файлы cookie, а также файлы cookie третьих лиц на нашем веб-сайте для улучшения вашего опыта, анализа нашего трафика, а также для обеспечения безопасности и маркетинга. Оставаясь на сайте Вы соглашаетесь с <Link to='/user_agreement' className="underline">Политикой использования файлов cookie</Link>.
         </p>
         <div className="flex justify-center md:justify-end text-sm">
-            <button 
-                onClick={handleAcceptAll} 
-                className="w-48 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-3xl mr-4"
-             >
-                Я согласен
-            </button>
+          <button 
+            onClick={handleAcceptAll} 
+            className="w-48 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-3xl mr-4"
+          >
+            Я согласен
+          </button>
         </div>
       </div> 
     </div>
