8b1b0b476f746f9a4f25586fdbf70e8f15116cf6
Pavel <p.volokh.a@gmail.com>
2024-11-08 14:55:59 +0300

    2 variant modalpage

--- src/components/modal/CookieModal.tsx
+++ src/components/modal/CookieModal.tsx
@@ -9,9 +9,6 @@ export const CookiePolicyModal: React.FC = () => {
     setShowModal(false);
   };
 
-  const handleManageSettings = () => {
-  };
-
   if (!showModal) {
     return null;
   } // Я заменю этот иф на что-то другое, когда узнаю, чё надо сделать, не трогай, пусть этот "условный мусор" останется
@@ -20,24 +17,30 @@ export const CookiePolicyModal: React.FC = () => {
     <div className="fixed bottom-0 left-0 w-full bg-gray-800 text-white p-4 z-50">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
         <p className="mb-4 md:mb-0 text-sm text-center md:text-left">
-          Мы используем собственные файлы cookie, а также файлы cookie третьих лиц на наших веб-сайтах для улучшения вашего опыта, анализа нашего трафика, а также для обеспечения безопасности и маркетинга. Выберите «Принять все», чтобы разрешить их использование. Прочитайте нашу <Link to = '/user_agreement' className="underline">Политику использования файлов cookie</Link>.
+          Мы используем собственные файлы cookie, а также файлы cookie третьих лиц на наших веб-сайтах для улучшения вашего опыта, анализа нашего трафика, а также для обеспечения безопасности и маркетинга. Оставаясь на сайте Вы соглашаетесь с <Link to = '/user_agreement' className="underline">Политикой использования файлов cookie</Link>
         </p>
         <div className="flex justify-center md:justify-end text-sm">
             <button 
-                onClick={handleManageSettings} 
-                className="text-white font-bold py-2 px-4"
-            >
-                Управление настройками
-            </button>
-            <button 
                 onClick={handleAcceptAll} 
-                className="w-48 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-3xl"
+                className="w-48 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-3xl mr-4"
              >
-                Принять все
+                Я согласен
             </button>
         </div>
+      </div> 
+      
+        {/* <p className="text-center mb-4">
+          Мы используем собственные файлы cookie, а также файлы cookie третьих лиц на наших веб-сайтах для улучшения вашего опыта, анализа нашего трафика, а также для обеспечения безопасности и маркетинга. Оставаясь на сайте Вы соглашаетесь с <Link to = '/user_agreement' className="underline">Политикой использования файлов cookie</Link>
+        </p>
+        <div className="flex justify-center">
+          <button 
+            onClick={handleAcceptAll} 
+            className="w-64 bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-6 rounded-3xl"
+          >
+            Я согласен
+          </button>
+        </div>*/}
       </div>
-    </div>
   );
 };
 
