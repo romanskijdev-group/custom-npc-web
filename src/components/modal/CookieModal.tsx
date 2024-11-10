f1827cde066ca9aaae76e16e76f3aa0a8de85e37
Pavel <p.volokh.a@gmail.com>
2024-11-10 16:06:32 +0300

    added translation

--- src/components/modal/CookieModal.tsx
+++ src/components/modal/CookieModal.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Cookies from 'js-cookie';
+import { useTranslation } from 'react-i18next';
 
 export const CookiePolicyModal: React.FC = () => {
   const [showModal, setShowModal] = useState(false);
+  const { t } = useTranslation();
 
   useEffect(() => {
     const cookiesBlock = Cookies.get('cookies_block');
@@ -19,20 +21,18 @@ export const CookiePolicyModal: React.FC = () => {
 
   if (!showModal) {
     return null;
-  } // Я заменю этот иф на что-то другое, когда узнаю, чё надо сделать, не трогай, пусть этот "условный мусор" останется
+  } // Я заменю этот иф на что-то другое, когда узнаю, чё надо сделать, не трогай, пусть этот "условный мусор" останется 
 
   return (
     <div className="fixed bottom-0 left-0 w-full bg-gray-800 text-white p-4 z-50 bg-opacity-75">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <p className="mb-4 md:mb-0 text-sm text-center md:text-left">
-          Мы используем собственные файлы cookie, а также файлы cookie третьих лиц на нашем веб-сайте для улучшения вашего опыта, анализа нашего трафика, а также для обеспечения безопасности и маркетинга. Оставаясь на сайте Вы соглашаетесь с <Link to='/user_agreement' className="underline">Политикой использования файлов cookie</Link>.
-        </p>
+        <p className="mb-4 md:mb-0 text-sm text-center md:text-left">{t('cookieModal.title')}<Link to='/user_agreement' className="underline">{t('cookieModal.hrefTitle')}</Link></p>
         <div className="flex justify-center md:justify-end text-sm">
           <button 
             onClick={handleAcceptAll} 
             className="w-48 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-3xl mr-4"
           >
-            Я согласен
+            {t('cookieModal.buttonTitle')}
           </button>
         </div>
       </div> 
